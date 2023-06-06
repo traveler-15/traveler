146ed4b30664195f6d1b4365ba8c62715aefca3f
hangju0610 <phj950610@naver.com>
2023-06-06 23:23:54 +0900

    search_place 및 select_map 구현

--- app.py
+++ app.py
@@ -13,26 +13,33 @@ def home():
 
 @app.route("/place/search", methods=["POST"])
 def place_search():
-    query_receive = request.form['query_give']#넘겨받은 여행지
+    query_receive = request.form['query_give'] #넘겨받은 여행지
     client_id = "mvz_k56LApeEKijNqLM7"
     client_secret = "oTc3GwSvzo"
 
     encText = urllib.parse.quote(query_receive)
     url = "https://openapi.naver.com/v1/search/local.json?query=" + encText + \
-                "&display=10"#받을 데이터 갯수
+                "&display=5" #받을 데이터 갯수
 
     request1 = urllib.request.Request(url)
     request1.add_header("X-Naver-Client-Id",client_id)
     request1.add_header("X-Naver-Client-Secret",client_secret)
     response = urllib.request.urlopen(request1)
     rescode = response.getcode()
-    if(rescode==200):#성공시 'result':데이터
+    if(rescode==200): #성공시 'result':데이터
         response_body = response.read()
+        # result를 배열 안에 객체 형태로 제작 부탁드립니다.
         result = json.loads(response_body)
-        return jsonify({'result': result})
-    else:#성공시 'result':'실패'
+        print(result['items'])
+        result2 = [{'title': '몽탄', 'link': 'http://www.mongtan.co.kr','address': '서울특별시 용산구 한강로1가 251-1', 'mapx': '33.3590628', 'mapy': '126.534361'},
+                  {'title': '포석로 소<b>갈비</b>찜', 'link': 'https://www.instagram.com/poseok_ro', 'category': '음식점>한식', 'description': '', 'telephone': '', 'address': '경상북도 경주시 황남동 228-1', 'roadAddress': '경상북도 경주시 포석로1068번길 22', 'mapx': '35.1795543', 'mapy': '129.0756416'},
+                  {'title': '우목정', 'link': 'http://blog.naver.com/kim5325167', 'category': '한식>육류,고기요리', 'description': '', 'telephone': '', 'address': '경기도 포천시 이동면 장암리 283', 'roadAddress': '경기도 포천시 이동 면 화동로 1974', 'mapx': '344792', 'mapy': '602563'},
+                  {'title': '소옥', 'link': 'http://instagram.com/so.ok_official', 'category': '음식점>한식', 'description': '', 'telephone': '', 'address': '경상북도 경주시 황남동 224-7 1층', 'roadAddress': '경상북도 경주시 포석로1050번길 29 1층', 'mapx': '509737', 'mapy': '360086'},
+                  {'title': '원조이동김미자할머니<b>갈비</b>  포천이동본점', 'link': 'http://www.김미자할머니갈비.kr/', 'category': '한식>육류,고기요리', 'description': '', 'telephone': '', 'address': '경기도 포천시 이동면 장암리 216-3', 'roadAddress': '경기도 포천시 이동면 화동로 2087', 'mapx': '344545', 'mapy': '603667'}
+                  ]
+        return jsonify(result['items'])
+    else: #성공시 'result':'실패'
         return jsonify({'result': "실패"})
-
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=3000, debug=True)
--- static/js/map.js
+++ static/js/map.js
@@ -1,36 +1,95 @@
 'use strict';
 
-      // 가고싶은 여행지 검색 
-function save_comment() {
-   let query = $("#name").val();
+// 저장되어 있는 여행지 보여주기
+const show_place = function() {
 
+}
+
+// 가고싶은 여행지 검색 및 리스트 띄우기
+const search_place = function () {
+   let query = $("#name").val();
    let formData = new FormData();
    formData.append("query_give", query);
 
-   fetch("/place/search", { method: "POST", body: formData })
+   fetch ("/place/search", { method: "POST", body: formData })
       .then((res) => res.json())
       .then((data) => {
-         console.log(data["result"]["items"]);
+         let rows = data
+         // 0번째 위치값 선정 후 좌표 전달
+         let first_place = new naver.maps.LatLng(rows[0]['mapx'], rows[0]['mapy'])
+         map.setCenter(first_place);
+
+         // find_search_${count} 숫자 다르게 하기
+         let count = 0
+         // 리스트 비워두기
+         $('#search_list').empty();
+         // rows 반복문 진행 -> 5개의 여행지 리스트 출력
+         rows.forEach((v) => {
+            let title = v['title'], // 이름
+            link = v['link'], // 링크
+            address = v['address'], // 주소
+            xMap = v['mapx'], // x좌표
+            yMap = v['mapy']; // y좌표
+            // temp_html 템플릿 출력하기
+            let temp_html =`<div class="card">
+                              <div class="card-body">
+                                 <blockquote class="blockquote mb-0">
+                                    <p>이름 : ${title} </p>
+                                    <a href = '#'><p id = "find_search_${count}" onclick='select_map(${count},${xMap},${yMap})'>위치 : ${address}</p></a>
+                                    <a href = "${link}"><p>${link}</p></a>
+                                 </blockquote>
+                              </div>
+                           </div>`
+            $('#search_list').append(temp_html);
+            count =+ 1
+         })
    });
 }
 
-let xMap = 37.3595704
-let yMap = 127.105399
+
+// 여행지 저장하기
+const save_map = function() {
+
+}
+// 여행지 찾기, 버튼 클릭했을 경우 해당 위치로 이동
+const select_map = function(count,xMap,yMap) {
+   var find_map = new naver.maps.LatLng(xMap, yMap)
+   $(`#find_search_${count}`).on("click", function(e) {
+      console.log(xMap,yMap)
+      e.preventDefault();
+      map.setCenter(find_map);
+   });
+}
+
+// 초기 Map 설정
+// let xMap = 37.3595704
+// let yMap = 127.105399
+
 
 // Map Option 기능
-const mapOptions = {
-   center: new naver.maps.LatLng(xMap, yMap),
+let mapOptions = {
+   center: new naver.maps.LatLng(37.3595704, 127.105399),
    zoomControl: true,
    zoomControlOptions: {
       style: naver.maps.ZoomControlStyle.SMALL,
       position: naver.maps.Position.TOP_RIGHT
    },
-   zoom: 15
+   zoom: 10
 };
 
+// Map을 templates에 띄우는 코드
 let map = new naver.maps.Map(document.querySelector("#map"), mapOptions);
 
-let marker = new naver.maps.Marker({
-   position: new naver.maps.LatLng(xMap, yMap),
-   map: map
-});
+
+// 버튼 입력하기 예제
+// $("#to-jeju").on("click", function(e) {
+//    e.preventDefault();
+
+//    map.setCenter(jeju);
+// });
+
+// let marker = new naver.maps.Marker({
+//    position: new naver.maps.LatLng(xMap, yMap),
+//    map: map
+// });
+
