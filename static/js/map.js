8cae1c01470b9f61884c81b5d6523c1ba86b14a2
hangju0610 <phj950610@naver.com>
2023-06-07 10:06:15 +0900

    gitignore 생성 및 JS 추가 수정

--- static/js/map.js
+++ static/js/map.js
@@ -14,13 +14,12 @@ const search_place = function () {
    fetch ("/place/search", { method: "POST", body: formData })
       .then((res) => res.json())
       .then((data) => {
-         let rows = data
+         let rows = data // 배열 안에 객체
+
          // 0번째 위치값 선정 후 좌표 전달
          let first_place = new naver.maps.LatLng(rows[0]['mapx'], rows[0]['mapy'])
          map.setCenter(first_place);
 
-         // find_search_${count} 숫자 다르게 하기
-         let count = 0
          // 리스트 비워두기
          $('#search_list').empty();
          // rows 반복문 진행 -> 5개의 여행지 리스트 출력
@@ -35,13 +34,12 @@ const search_place = function () {
                               <div class="card-body">
                                  <blockquote class="blockquote mb-0">
                                     <p>이름 : ${title} </p>
-                                    <a href = '#'><p id = "find_search_${count}" onclick='select_map(${count},${xMap},${yMap})'>위치 : ${address}</p></a>
+                                    <a href = '#'><p id = "find_search" onclick='select_map(${xMap},${yMap})'>위치 : ${address}</p></a>
                                     <a href = "${link}"><p>${link}</p></a>
                                  </blockquote>
                               </div>
                            </div>`
             $('#search_list').append(temp_html);
-            count =+ 1
          })
    });
 }
@@ -51,15 +49,12 @@ const search_place = function () {
 const save_map = function() {
 
 }
+
 // 여행지 찾기, 버튼 클릭했을 경우 해당 위치로 이동
-const select_map = function(count,xMap,yMap) {
+const select_map = function(xMap,yMap) {
    var find_map = new naver.maps.LatLng(xMap, yMap)
-   $(`#find_search_${count}`).on("click", function(e) {
-      console.log(xMap,yMap)
-      e.preventDefault();
-      map.setCenter(find_map);
-   });
-}
+   map.setCenter(find_map);
+   };
 
 
 // 초기 Map 설정
@@ -75,7 +70,7 @@ let mapOptions = {
       style: naver.maps.ZoomControlStyle.SMALL,
       position: naver.maps.Position.TOP_RIGHT
    },
-   zoom: 10
+   zoom: 13
 };
 
 // Map을 templates에 띄우는 코드
